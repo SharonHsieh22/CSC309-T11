228d701da903279362fa10f06f7708c85c607531
Sharon Hsieh <sharonyt22@gmail.com>
2026-03-29 15:48:50 -0400

    ok

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import routes from "./routes.js";
 // TODO: complete me (loading the necessary packages)
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
